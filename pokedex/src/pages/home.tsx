dda34ef03b0973ad3e702e66aecabf6d2978f58b
raphael <rafavfx1@gmail.com>
2024-04-24 22:30:28 -0300

    adicionando os cards no grid

--- pokedex/src/pages/home.tsx
+++ pokedex/src/pages/home.tsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import PokemonCard from '../components/card.tsx'
+import NavBar from '../components/navbar.tsx'
+import { Container, Grid } from '@mui/material'
 
 export const Home = () => {
     return( 
-        <div>HOME</div>
+        <div>
+            <NavBar>
+                
+            </NavBar>
+            <Container maxWidth={false}>
+                <Grid container>
+                    <Grid item xs={3}>
+                        <PokemonCard></PokemonCard>
+                        <PokemonCard></PokemonCard>
+                        <PokemonCard></PokemonCard>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <PokemonCard></PokemonCard>
+                        <PokemonCard></PokemonCard>
+                        <PokemonCard></PokemonCard>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <PokemonCard></PokemonCard>
+                        <PokemonCard></PokemonCard>
+                        <PokemonCard></PokemonCard>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <PokemonCard></PokemonCard>
+                        <PokemonCard></PokemonCard>
+                        <PokemonCard></PokemonCard>
+                    </Grid>
+                    
+                </Grid>
+                
+            </Container>
+        </div>
     )
 }
